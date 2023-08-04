7b382d0ac2dfc509b1b1724fcfe039c2f1f01071
kelvinokumu <oumakelvinokumu@gmail.com>
2023-08-04 16:41:02 +0300

    cleaning

--- sql_scripts/cleaning.sql
+++ sql_scripts/cleaning.sql
@@ -45,7 +45,43 @@
 -- FROM taxi_trips_2017
 -- WHERE passenger_count <= 0;
 
-SELECT *
+
+-- ALTER TABLE taxi_trips_2017
+-- ADD COLUMN payment_type_description text;
+
+-- UPDATE taxi_trips_2017
+-- SET payment_type_description =
+--     CASE
+--         WHEN payment_type = 1 THEN 'Credit card'
+--         WHEN payment_type = 2 THEN 'Cash'
+--         WHEN payment_type = 3 THEN 'No charge'
+--         WHEN payment_type = 4 THEN 'Dispute'
+--         WHEN payment_type = 5 THEN 'Unknown'
+--         WHEN payment_type = 6 THEN 'Voided trip'
+--         ELSE 'Invalid'
+--     END;
+
+
+-- ALTER TABLE taxi_trips_2017
+-- ADD COLUMN trip_type_description text;
+
+-- UPDATE taxi_trips_2017
+-- SET trip_type_description =
+--     CASE
+--         WHEN trip_type = 1 THEN 'Street-hail'
+--         WHEN trip_type = 2 THEN 'Dispatch'
+--         ELSE 'Invalid'
+--     END;
+
+SELECT trip_type_description, COUNT(*) AS trip_count
 FROM taxi_trips_2017
-LIMIT 5;
+GROUP BY trip_type_description;
+
+-- SELECT trip_type, COUNT(*) AS trip_count
+-- FROM taxi_trips_2017
+-- GROUP BY trip_type;
+
+-- SELECT *
+-- FROM taxi_trips_2017
+-- LIMIT 5;
 

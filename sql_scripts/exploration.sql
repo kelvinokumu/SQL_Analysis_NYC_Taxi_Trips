1e71b351d48716554a69469f5955c23715ab20d3
kelvinokumu <oumakelvinokumu@gmail.com>
2023-08-31 10:11:26 +0300

    comments

--- sql_scripts/exploration.sql
+++ sql_scripts/exploration.sql
@@ -1,67 +1,67 @@
--- Calculate Average Fare Amount
+-- Calculate the average fare amount
 SELECT AVG(fare_amount) 
 FROM taxi_trips_2017;
 
--- Calculate Total Trip Distance
+-- Calculate the total trip distance
 SELECT SUM(trip_distance) 
 FROM taxi_trips_2017;
 
--- Identify Longest and Shortest Trips
+-- Identify the longest and shortest trips
 SELECT MAX(trip_duration) AS longest_trip, MIN(trip_duration) AS shortest_trip
 FROM taxi_trips_2017;
 
--- Check Store and Forward Flags
+-- Check store and forward flags
 SELECT COUNT(*) FROM taxi_trips_2017 WHERE store_and_fwd_flag = true;
 
--- Calculate Total Tip Amount and Average Tip Percentage
+-- Calculate the total tip amount and average tip percentage
 SELECT SUM(tip_amount) AS total_tip_amount,
       (SUM(tip_amount) / SUM(fare_amount)) * 100 AS avg_tip_percentage
 FROM taxi_trips_2017;
 
--- Passenger count per hour
+-- Count trips per hour
 SELECT EXTRACT(HOUR FROM lpep_pickup_datetime) AS hour, COUNT(*) AS trip_count
 FROM taxi_trips_2017
 GROUP BY hour
 ORDER BY trip_count DESC;
 
--- Average Tip Percentage by Payment Type
+-- Calculate average tip percentage by payment type
 SELECT payment_type_description, AVG((tip_amount / fare_amount) * 100) AS avg_tip_percentage
 FROM taxi_trips_2017
 WHERE fare_amount > 0
 GROUP BY payment_type_description;
 
--- Common Payment type
+-- Find common payment types
 SELECT payment_type_description, COUNT(*) AS count
 FROM taxi_trips_2017
 WHERE fare_amount > 0
 GROUP BY payment_type_description
 ORDER BY count DESC;
 
--- Common Trip type Count
+-- Find common trip types and their counts
 SELECT trip_type_description, COUNT(*) AS count
 FROM taxi_trips_2017
 WHERE fare_amount > 0
 GROUP BY trip_type_description
 ORDER BY count DESC;
 
--- Trip count by Month
+-- Count trips by month
 SELECT EXTRACT(MONTH FROM lpep_pickup_datetime) AS month, COUNT(*) AS trip_count
 FROM taxi_trips_2017
 GROUP BY month
 ORDER BY trip_count DESC;
 
--- calculates the count of trips for each unique passenger count
+-- Calculate the count of trips for each unique passenger count
 SELECT passenger_count, COUNT(*) AS passenger_counts
 FROM taxi_trips_2017
 GROUP BY passenger_count
 ORDER BY passenger_counts DESC;
 
--- Average fare for different user count.
+-- Calculate average fare for different passenger counts
 SELECT passenger_count, AVG(fare_amount) AS avg_fare
 FROM taxi_trips_2017
 GROUP BY passenger_count;
 
--- Best and Worst pickup locations
+-- Find the best and worst pickup locations
 WITH pickup_counts AS (
     SELECT pulocationid, COUNT(*) AS pickup_count
     FROM taxi_trips_2017

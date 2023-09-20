89f170982d77eacd79e7c9a1a73a272edac889b7
kelvinokumu <oumakelvinokumu@gmail.com>
2023-09-20 16:59:13 +0300

    edited some queries

--- sql_scripts/exploration.sql
+++ sql_scripts/exploration.sql
@@ -1,31 +1,42 @@
 -- Calculate the average fare amount
-SELECT AVG(fare_amount) 
+SELECT ROUND(AVG(fare_amount)::NUMERIC ,2) AS avg_fare
 FROM taxi_trips_2017;
 
 -- Calculate the total trip distance
-SELECT SUM(trip_distance) 
+SELECT ROUND(SUM(trip_distance)::NUMERIC, 2) AS total_distance_covered
 FROM taxi_trips_2017;
 
 -- Identify the longest and shortest trips
 SELECT MAX(trip_duration) AS longest_trip, MIN(trip_duration) AS shortest_trip
-FROM taxi_trips_2017;
+FROM taxi_trips_2017
+WHERE EXTRACT(EPOCH FROM trip_duration) > 0;
 
 -- Check store and forward flags
-SELECT COUNT(*) FROM taxi_trips_2017 WHERE store_and_fwd_flag = true;
-
--- Calculate the total tip amount and average tip percentage
-SELECT SUM(tip_amount) AS total_tip_amount,
-      (SUM(tip_amount) / SUM(fare_amount)) * 100 AS avg_tip_percentage
+SELECT 
+  SUM(CASE WHEN store_and_fwd_flag = true THEN 1 ELSE 0 END) AS true_count,
+  SUM(CASE WHEN store_and_fwd_flag = false THEN 1 ELSE 0 END) AS false_count
 FROM taxi_trips_2017;
 
+-- Calculate the total tip amount and average tip percentage
+WITH TipSums AS (
+  SELECT 
+    CAST(SUM(tip_amount) AS numeric) AS total_tip_amount, 
+    CAST(SUM(fare_amount) AS numeric) AS total_fare_amount
+  FROM taxi_trips_2017
+)
+SELECT 
+  ROUND(total_tip_amount, 2) AS total_tip_amount,
+  ROUND((total_tip_amount / total_fare_amount) * 100, 2) AS avg_tip_percentage
+FROM TipSums;
+
 -- Count trips per hour
-SELECT EXTRACT(HOUR FROM lpep_pickup_datetime) AS hour, COUNT(*) AS trip_count
+SELECT EXTRACT(HOUR FROM lpep_pickup_datetime) AS pickup_hour, COUNT(*) AS trip_count
 FROM taxi_trips_2017
-GROUP BY hour
+GROUP BY pickup_hour
 ORDER BY trip_count DESC;
 
 -- Calculate average tip percentage by payment type
-SELECT payment_type_description, AVG((tip_amount / fare_amount) * 100) AS avg_tip_percentage
+SELECT payment_type_description, ROUND(AVG((tip_amount / fare_amount) * 100)::numeric, 2) AS avg_tip_percentage
 FROM taxi_trips_2017
 WHERE fare_amount > 0
 GROUP BY payment_type_description;
@@ -40,7 +51,7 @@ ORDER BY count DESC;
 -- Find common trip types and their counts
 SELECT trip_type_description, COUNT(*) AS count
 FROM taxi_trips_2017
-WHERE fare_amount > 0
+WHERE fare_amount > 0 AND trip_type_description NOT IN ('Invalid', 'N/A')
 GROUP BY trip_type_description
 ORDER BY count DESC;
 
